55dd731912d9785802f397c6dc61b0269105a5e3
Dustin Weatherford <dustin@tenxer.com>
2012-02-11 17:53:38 -0800

    Initialization of wwat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wwat::Application.config.secret_token = 'd0517aa9e966a8fd836774152151e59e9854813c0eadac6f55ab576969d2f49b10730a4a60aeac61759d7d64e6e12e9d00eb241429471bd6e37e9099fb8a23ea'
